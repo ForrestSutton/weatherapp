95a4110a34cc40cb496f82667e96ebfe636b491a
Forrest Sutton <forrestsutton@gmail.com>
2018-04-19 13:46:11 -0700

    working app, need to fix time by location

--- app.py
+++ app.py
@@ -1,25 +1,53 @@
 import requests
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+import time
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://weather.db'
 
-@app.route('/')
+
+db = SQLAlchemy(app)
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
-    city = 'Las Vegas'
-    app_id = 'x'
-    r = requests.get(url.format(city)(app_id)).json()
-    print(r)
+    if request.method == 'POST':
+        new_city = request.form.get('city')
 
-    weather = {
-        'city': city,
-        'tempature' : r['main']['temp'] ,
-        'description': r['weather'][0]['description'],
-        'icon' : r['weather'][0]['icon'],
+        if new_city:
+            new_city_obj = City(name=new_city)
+
+            db.session.add(new_city_obj)
+            db.session.commit()
+
+    now = time.strftime('%l:%M %p %Z')
+
+    cities = City.query.all()
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=<KEY>'
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city.name)).json()
+
+        weather = {
+            'city' : city.name,
+            'time': now,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(weather)
 
 
-            }
-
-    return render_template('weather.html')
+    return render_template('weather.html', weather_data=weather_data)
